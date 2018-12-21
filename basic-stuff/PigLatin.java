8c2e1f373b960ca86f288ab08750d48db635413b
Aram Moghaddassi <moghadddassi.aram@gmail.com>
2018-12-21 12:12:34 -0500

    fixed piglatin.

--- basic-stuff/PigLatin.java
+++ basic-stuff/PigLatin.java
@@ -9,18 +9,22 @@ class PigLatin {
 	System.out.println("");
 	}
 
-	/**Converts input into pig latin (moving all consenants to back, and adding ay*/
 	private static String pigLatin(String input) {
-		List vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
-		String front = "";
-		String back = "";
+		int splitPoint = leadingConsenents(input);
+		String front = input.substring(splitPoint, input.length());
+		String back = input.substring(0, splitPoint);
+		return front + back + "ay";		
+	}
+
+private static int leadingConsenents(String input) {
+		int counter = 0;
 		for (int i=0;i<input.length();i++) {
-			if (vowels.contains(input.charAt(i))) {
-				front = front+input.charAt(i);
-			} else {
-				back = back+input.charAt(i);
-			}
-		}
-		return front+back+"ay";
+			if (vowel(input.charAt(i))) return counter;
+			else counter = counter+1;
+		} return counter;
+	}
+
+	private static boolean vowel(char x) {
+		return x == 'a' || x == 'e'|| x == 'i'|| x == 'o'|| x == 'u';
 	}
 }
